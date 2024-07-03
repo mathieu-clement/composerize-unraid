d596df29baa278e11065e56c5a2cd84ffb7a43ea
Mathieu Clement <mathieu.clement@fairtiq.com>
2024-07-04 00:37:47 +0200

    add a few more tests for docker.Lines

--- internal/docker/docker_test.go
+++ internal/docker/docker_test.go
@@ -17,3 +17,28 @@ func TestLinesWithTrailingLineBreak(t *testing.T) {
         t.Fatalf("expected '%v', actual '%v'", expected, actual)
     }
 }
+
+func TestLinesWithoutTrailingLineBreak(t *testing.T) {
+    input := "This is a multiline string.\nIt spans multiple lines."
+    expected := []string {
+        "This is a multiline string.",
+        "It spans multiple lines.",
+    }
+    actual := Lines(input)
+
+    if !reflect.DeepEqual(expected, actual) {
+        t.Fatalf("expected '%v', actual '%v'", expected, actual)
+    }
+}
+
+func TestLinesWithNoLineBreaks(t *testing.T) {
+    input := "This is a one-line string."
+    expected := []string {
+        "This is a one-line string.",
+    }
+    actual := Lines(input)
+
+    if !reflect.DeepEqual(expected, actual) {
+        t.Fatalf("expected '%v', actual '%v'", expected, actual)
+    }
+}
